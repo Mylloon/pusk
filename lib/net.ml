3748b72e6a2fe45bffe04e232ffa0788715b7735
Mylloon <kennel.anri@tutanota.com>
2023-05-13 10:40:13 +0200

    easier interface

--- lib/net.ml
+++ lib/net.ml
@@ -5,5 +5,10 @@ let send_post_request url json =
   let headers = Cohttp.Header.init_with "Content-Type" "application/json" in
   let body = Cohttp_lwt.Body.of_string json in
   Lwt.bind (Client.post ~headers ~body uri) (fun (_response, body) ->
-      (* Lwt.map (fun body_str -> response, body_str) *) Cohttp_lwt.Body.to_string body)
+      Cohttp_lwt.Body.to_string body)
+;;
+
+let execute_request url json =
+  let body = send_post_request url json in
+  Lwt_main.run body
 ;;
